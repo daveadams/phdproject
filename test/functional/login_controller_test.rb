4e9c27e00e1e431cfecc0247b0183d5ec121568b
daveadams <daveadams@gmail.com>
2009-08-09 15:22:03 -0400

    Added basic functional tests for login and tutorial controllers

--- test/functional/login_controller_test.rb
+++ test/functional/login_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class LoginControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "get index" do
+    get :index
+    assert_response :success
+    assert_select "title", "Login"
+    # TODO: no warning message should appear at this point
+  end
+
+  test "simple post" do
+    post :index
+    assert_response :redirect
+    assert_redirected_to(:controller => "tutorial")
   end
 end
