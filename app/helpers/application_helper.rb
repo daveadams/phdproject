82eb193911f145158f24835c7c3fc1543202c877
daveadams <daveadams@gmail.com>
2009-08-09 14:31:40 -0400

    application_helper.rb: added link_button method for GET buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def link_button(button_text, url_options = {})
+    form_tag(url_options, :method => "get", :style => "display:inline") +
+      "<input type=\"submit\" value=\"#{button_text}\" /></form>"
+  end
 end
