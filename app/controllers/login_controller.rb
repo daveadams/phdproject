a6331cfb36b87696837746f36de8291547ed53bd
daveadams <daveadams@gmail.com>
2009-08-12 15:06:37 -0400

    login_controller.rb: now handles login, session startup; also error text changes were made

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,9 +3,15 @@ class LoginController < ApplicationController
     if request.post?
       participant = Participant.find_active(request[:participant_number])
       if participant.nil?
-        flash[:error] = "Invalid participant number."
+        flash[:error] = "Invalid participant number. Please try again."
       else
-        redirect_to(:controller => "tutorial")
+        begin
+          participant.login
+          session[:participant_id] = participant.id
+          redirect_to(:controller => "tutorial")
+        rescue ParticipantAlreadyActive
+          flash[:error] = "That participant number is already in use. Please notify the staff."
+        end
       end
     end
   end
