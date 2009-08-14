85dc27e2136b112960138028bf04597a3e8811b2
daveadams <daveadams@gmail.com>
2009-08-14 16:18:15 -0400

    experimental_session.rb: added create_participants method and corresponding tests

--- app/models/experimental_session.rb
+++ app/models/experimental_session.rb
@@ -5,4 +5,15 @@ class ExperimentalSession < ActiveRecord::Base
   def is_active?
     Time.now >= starts_at and Time.now <= ends_at
   end
+
+  def create_participants(n, group)
+    properties = {
+      :experimental_session => self,
+      :experimental_group => ExperimentalGroup.find(group)
+    }
+    spec = []
+    n.times { spec << properties }
+
+    Participant.create(spec)
+  end
 end
--- test/unit/experimental_session_test.rb
+++ test/unit/experimental_session_test.rb
@@ -6,6 +6,9 @@ class ExperimentalSessionTest < ActiveSupport::TestCase
                                 :starts_at => 3.days.ago.to_s(:db),
                                 :ends_at => 2.days.ago.to_s(:db))
     assert !x.is_active?
+
+    assert x.valid?
+    assert x.save
   end
 
   test "new active" do
@@ -13,5 +16,27 @@ class ExperimentalSessionTest < ActiveSupport::TestCase
                                 :starts_at => 1.days.ago.to_s(:db),
                                 :ends_at => 1.days.from_now.to_s(:db))
     assert x.is_active?
+
+    assert x.valid?
+    assert x.save
+  end
+
+  test "create participants" do
+    x = ExperimentalSession.new(:name => "A Test Session",
+                                :starts_at => 1.days.ago.to_s(:db),
+                                :ends_at => 1.days.from_now.to_s(:db))
+    assert x.valid?
+    assert x.save
+
+    n = 10
+    participants = x.create_participants(n, experimental_groups(:control))
+
+    assert_equal(participants.length, n)
+    participants.each do |p|
+      assert_equal(p.experimental_session, x)
+      assert_equal(p.experimental_group, experimental_groups(:control))
+      assert(p.id)
+      assert_equal(Participant.find(p.id), p)
+    end
   end
 end
