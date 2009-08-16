634c1bcbd8f9e4d5d3a0900b43a5b00292f8c312
daveadams <daveadams@gmail.com>
2009-08-16 14:22:38 -0400

    Adjusted tutorial actions, created corresponding views, and updated page_order fixtures to correspond to correct tutorial page structure

--- app/controllers/tutorial_controller.rb
+++ app/controllers/tutorial_controller.rb
@@ -4,10 +4,28 @@ class TutorialController < ApplicationController
   end
 
   def intro; end
-  def earnings; end
-  def taxes; end
-  def report; end
-  def audit; end
-  def doublecheck; end
+  def overview; end
+
+  def earnings_intro; end
+  def earnings_task; end
+  def earnings_report; end
+
+  def tax_intro; end
+  def tax_return; end
+
+  def disclosure_intro; end
+  def disclosure_report; end
+
+  def audit_intro; end
+  def audit_notify; end
+  def audit_ok; end
+  def audit_error; end
+
+  def doublecheck_intro; end
+  def doublecheck_notify; end
+  def doublecheck_ok; end
+  def doublecheck_error; end
+
+  def completing; end
   def complete; end
 end
