51f67293afe6cc7735bedf516f5c2d5ee4836974
daveadams <daveadams@gmail.com>
2009-08-16 08:59:09 -0400

    Complete controller skeleton and functional test

--- app/controllers/complete_controller.rb
+++ app/controllers/complete_controller.rb
@@ -1,5 +1,7 @@
 class CompleteController < ApplicationController
   def index
+    # TODO: check if @participant completed everything
+    #       If so, mark completed_all and kill session
+    #       Otherwise raise an error
   end
-
 end
