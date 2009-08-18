b8164c4086cb15d107e29d44afdbb9118e656c48
daveadams <daveadams@gmail.com>
2009-08-17 22:18:06 -0400

    experiment_controller.rb: added an action and a couple of future notes

--- app/controllers/experiment_controller.rb
+++ app/controllers/experiment_controller.rb
@@ -2,6 +2,11 @@ class ExperimentController < ApplicationController
   def index
   end
 
+  def wait
+    # TODO: add reload to layout for this action
+    # TODO: check phase/round status and advance as appropriate
+  end
+
   def complete
     @participant.experiment_complete = true
     @participant.save
