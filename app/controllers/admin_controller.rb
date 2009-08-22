591f8177ae5e29d4586c3d2691d0137bf0878ad4
daveadams <daveadams@gmail.com>
2009-08-21 23:11:10 -0400

    sessions.html.erb, admin_controller.rb: major improvements - partial for session participant count rendering, addition of add_participant functionality

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -15,6 +15,7 @@ class AdminController < ApplicationController
 
     @active_session = ExperimentalSession.active
     @sessions = ExperimentalSession.find(:all) - [@active_session]
+    @groups = ExperimentalGroup.find(:all)
   end
 
   def activate_session
@@ -63,7 +64,56 @@ class AdminController < ApplicationController
     redirect_to(:action => :sessions)
   end
 
-  def create_participants
+  def add_participants
+    @page_title = "Add Participants"
+
+    begin
+      @session = ExperimentalSession.find(request[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "Could not find that experimental session."
+      redirect_to(:action => :sessions)
+    end
+
+    if @session.is_locked_down
+      flash[:error] = "Can't add participants to a locked-down session."
+      redirect_to(:action => :sessions)
+    end
+
+    if @session.is_complete
+      flash[:error] = "Can't add participants to a completed session."
+      redirect_to(:action => :sessions)
+    end
+
+    if request.post?
+      n = request[:n].nil? ? nil : request[:n].to_i
+      if n.nil?
+        flash[:error] = "You must provide a number of participants to create."
+        redirect_to(:action => :add_participants, :id => @session.id)
+      elsif request[:experimental_group_id].nil?
+        flash[:error] = "Please select an experimental group."
+        redirect_to(:action => :add_participants, :id => @session.id)
+      elsif n <= 0
+        flash[:error] = "Please specify a number greater than zero."
+        redirect_to(:action => :add_participants, :id => @session.id)
+      elsif n > 100
+        flash[:error] = "Cannot create more than 100 participants at once."
+        redirect_to(:action => :add_participants, :id => @session.id)
+      else
+        begin
+          @group = ExperimentalGroup.find(request[:experimental_group_id].to_i)
+          @session.create_participants(n, @group.id)
+          redirect_to(:action => :sessions)
+        rescue ActiveRecord::RecordNotFound
+          flash[:error] = "Invalid experimental group selected."
+          redirect_to(:action => :add_participants, :id => @session.id)
+        rescue => e
+          flash[:error] = "An unexpected error occured when trying to add the participants: #{e}"
+          redirect_to(:action => :add_participants, :id => @session.id)
+        end
+      end
+    else
+      @groups = ExperimentalGroup.all
+    end
   end
 
   def lockdown
