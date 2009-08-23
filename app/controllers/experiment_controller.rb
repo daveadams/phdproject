921efcdd57091563c6d7321c5058ade1013f3235
daveadams <daveadams@gmail.com>
2009-08-23 18:16:22 -0400

    Adding views and skeleton actions for full experiment cycle

--- app/controllers/experiment_controller.rb
+++ app/controllers/experiment_controller.rb
@@ -2,29 +2,99 @@ class ExperimentController < ApplicationController
   before_filter :check_round, :except => [:index, :wait]
 
   def index
-    if @participant.tutorial_complete
-      if not @participant.experiment_complete
-        redirect_to(:action => :wait)
-      else
-        redirect_to(:controller => :survey)
-      end
-    end
+    redirect_to(:action => :wait)
   end
 
   def wait
     if @participant.experimental_session.phase == "experiment" and
         @participant.experimental_session.round == @participant.round
-      redirect_to(:action => :start_round)
+      redirect_to(:action => :begin)
     else
       render :action => :wait, :layout => false
     end
   end
 
-  def start_round
-    redirect_to(:action => :task)
+  def begin
+    @page_title = "Begin Round #{@participant.round}"
+    @display_bank = true
   end
 
-  def task
+  def work
+    @page_title = "Earnings Task"
+
+    # TODO: pull working text based on round from the database
+    @working_text = "This is filler text for round #{@participant.round}."
+
+    # TODO: autotimer
+  end
+
+  def earnings
+    # TODO: check timing, too
+    # TODO: actually check work
+    # TODO: summarize results into an object in the session
+    # TODO: add new money to bank
+    @page_title = "Earnings Report"
+    @display_bank = true
+    # TODO: pull real fixes from session object OR merge this and check_work
+    @fixes = ["Filler fix 1","Filler fix 2","Filler fix 3"]
+    # TODO: calculate total earned
+    @total_earned = @fixes.length * @participant.experimental_group.earnings
+
+    # TODO: calculate labels based on experimental_group
+    @earnings_per_correction_label = "Income Per Correction"
+    @total_earned_label = "Income Earned"
+  end
+
+  def message
+    # TODO: determine the message or whether to skip based on group
+    render :layout => false
+  end
+
+  def report
+    # TODO: page title based on group
+    @page_title = "Individual Income Tax Return"
+    # TODO: form labels based on group
+    @reported_earnings_label = "Reported Income"
+    @tax_rate_label = "Tax Rate"
+    @estimate_button_label = "Estimate Taxes"
+    @amount_due_label = "Tax Due"
+    @report_earnings_button_label = "Report Earnings"
+  end
+
+  def check
+    # TODO: select if this person gets audited or not
+    @page_title = "Notification"
+
+    # TODO: message based on group
+    @notification = "Your Individual Income Tax Return has been selected for audit. Please proceed to the audit report to see the results of the audit."
+
+    render :layout => false
+  end
+
+  def results
+    # TODO: title based on group
+    @page_title = "Audit Report"
+    @display_bank = true
+
+    # TODO: labels based on group
+    @earnings_label = "Earned Income"
+    @reported_label = "Reported Income"
+    @tax_rate_label = "Tax Rate"
+    @additional_label = "Additional Tax Due"
+
+    # TODO: set results_ok based on actual results
+    @results_ok = false
+
+    # TODO: need a way to keep track of current earnings data (session?)
+    @earned = 1.05
+    @reported = 0.8
+    @difference = 0.25
+    @additional = 0.05
+    @penalties = 0.07
+    @total = 0.12
+
+    # TODO: set message based on group
+    @message = "You owe $#{@total} in penalties and unpaid taxes. This amount has been subtracted from your bank."
   end
 
   def end_round
@@ -43,6 +113,9 @@ class ExperimentController < ApplicationController
       @participant.save
 
       redirect_to(:controller => "survey")
+    else
+      @page_title = "Work Complete"
+      @display_bank = true
     end
   end
 
--- app/helpers/experiment_helper.rb
+++ app/helpers/experiment_helper.rb
@@ -1,2 +1,5 @@
 module ExperimentHelper
+  def bank_balance
+    sprintf("%0.2f", @participant.cash)
+  end
 end
