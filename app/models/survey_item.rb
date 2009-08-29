ff3a130b8fa3662207f59a499a313d18b6bc60ad
daveadams <daveadams@gmail.com>
2009-08-29 15:56:55 -0400

    More linkage between survey_item and survey_page

--- app/models/survey_item.rb
+++ app/models/survey_item.rb
@@ -1,2 +1,4 @@
 class SurveyItem < ActiveRecord::Base
+  belongs_to :survey_page
+  belongs_to :question
 end
--- app/models/survey_page.rb
+++ app/models/survey_page.rb
@@ -1,4 +1,5 @@
 class SurveyPage < ActiveRecord::Base
   belongs_to :survey
-  belongs_to :depends_on_question, :class_name => "Question"
+  belongs_to :depends_on_answer, :class_name => "Answer"
+  has_many :questions, :through => :survey_items, :order => "sequence"
 end
