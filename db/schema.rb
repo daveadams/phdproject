63cda1e0113b2fc9eb3d2c7855f93f64dce70c6b
daveadams <daveadams@gmail.com>
2009-08-29 16:00:24 -0400

    Created Answer model, linked to Question

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090829195724) do
+ActiveRecord::Schema.define(:version => 20090829195932) do
 
   create_table "activity_logs", :force => true do |t|
     t.integer  "participant_id"
@@ -17,6 +17,13 @@ ActiveRecord::Schema.define(:version => 20090829195724) do
     t.string   "controller"
     t.string   "action"
     t.text     "details"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "answers", :force => true do |t|
+    t.integer  "question_id"
+    t.text     "answer"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
