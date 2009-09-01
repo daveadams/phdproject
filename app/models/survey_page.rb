95f64ed8a3037c364e2c30322bababf0aa520195
daveadams <daveadams@gmail.com>
2009-08-31 20:35:32 -0400

    Finished survey fixtures for initial two surveys

--- app/models/survey_page.rb
+++ app/models/survey_page.rb
@@ -2,5 +2,5 @@ class SurveyPage < ActiveRecord::Base
   belongs_to :survey
   belongs_to :depends_on_answer, :class_name => "Answer"
   has_many :items, :class_name => "SurveyItem", :order => "sequence"
-  has_many :questions, :through => :items
+  has_many :questions, :through => :items, :order => "sequence"
 end
